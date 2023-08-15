57aec91f9212a3508fd02e9f8930c6c8a70ee699
Donsidious <137007350+Donsidious@users.noreply.github.com>
2023-08-14 21:08:22 -0600

    Add files via upload

--- weather.js
+++ weather.js
@@ -1,18 +1,39 @@
-const forecastEl = document.getElementById("forecast")
+const forecastEl = document.getElementById("forecast");
 
-var requestOptions = {
-    method: 'GET',
-    redirect: 'follow'
-  };
-  
-  fetch("https://api.openweathermap.org/data/2.5/forecast?q=Denver,CoL&appid=0cd1c241fa9f2ab743c401cd5274afbb&cnt=3&units=imperial", requestOptions)
-    .then(response => response.json())
-    .then(data => {
-        console.log(data)
-        for (let index = 0; index < data.list.length; index++) {
-            const element = data.list[index];
-            console.log(element.weather[0].description)
-            forecastEl.textContent = forecastEl.textContent + element.weather[0].description +  ", "
-        }
-    })
-    .catch(error => console.log('error', error));
+// Fetch weather forecast data
+const weatherRequestOptions = {
+  method: 'GET',
+  redirect: 'follow'
+};
+
+fetch("https://api.openweathermap.org/data/2.5/forecast?q=Denver,CoL&appid=6f23fb377d0b5b11d15dd42bdf09fd1f&cnt=3&units=imperial", weatherRequestOptions)
+  .then(response => response.json())
+  .then(weatherData => {
+    console.log(weatherData);
+    for (let index = 0; index < weatherData.list.length; index++) {
+      const weatherElement = weatherData.list[index];
+      console.log(weatherElement.weather[0].description);
+      forecastEl.textContent = forecastEl.textContent + weatherElement.weather[0].description + ", ";
+    }
+
+    // Fetch public holidays data
+    const holidaysRequestOptions = {
+      method: 'GET',
+      redirect: 'follow'
+    };
+
+    fetch("https://date.nager.at/api/v3/PublicHolidays/2023/AT", holidaysRequestOptions)
+      .then(response => response.json())
+      .then(holidaysData => {
+        console.log(holidaysData);
+        
+        // Update HTML with public holidays data
+        const holidaysEl = document.getElementById("holidays");
+        holidaysData.forEach(holiday => {
+          const holidayDate = new Date(holiday.date).toLocaleDateString();
+          holidaysEl.innerHTML += `<p>${holidayDate}: ${holiday.name}</p>`;
+        });
+      })
+      .catch(error => console.log('Error fetching holidays:', error));
+  })
+  .catch(error => console.log('Error fetching weather:', error));
